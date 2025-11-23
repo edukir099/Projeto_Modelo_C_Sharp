aaca70014d54b430afcd5c8d649eeb8b61123486
Alan-Nasciment0 <alandonascimento2014@gmail.com>
2025-11-23 10:12:36 -0300

    Adicionando pasta DAO, conexao com banco de dados

---
+++ Projeto_Modelo_C_Sharp/DAO/MySQL_Conexao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Modelo_C_Sharp.DAO
+{
+    internal class MySQL_Conexao
+    {
+        public MySqlConnection String_Conexao()
+        {
+            return new MySqlConnection("user id=root;password=root;server=localhost:3307;database=bd_limpeza;SslMode=none");
+        }
+    }
+}
